8a1d9cbc52ba245ebe4fe7ca9a78a98bb50d897b
iryna.soltyk <Iryna.Soltyk@cantire.com>
2024-04-20 18:53:22 +0300

    change trigger to service bus

--- src/main/java/petstore/soltyk/Function.java
+++ src/main/java/petstore/soltyk/Function.java
@@ -1,36 +1,28 @@
 package petstore.soltyk;
 
-import java.util.Optional;
-
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.OutputBinding;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.BlobOutput;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
 import com.microsoft.azure.functions.annotation.StorageAccount;
 
 public class Function {
     @FunctionName("resolveOrder")
     @StorageAccount("AZURE_STORAGE")
     public void run(
-            @HttpTrigger(
-                    name = "req",
-                    methods = {HttpMethod.POST},
-                    authLevel = AuthorizationLevel.ANONYMOUS)
-                    HttpRequestMessage<Optional<String>> request,
+            @ServiceBusQueueTrigger(
+                    name = "msg",
+                    queueName = "%SB_QUEUE_NAME%",
+                    connection = "%SB_CONNECTION_STRING%")
+                    String message,
             @BlobOutput(
                     name = "output",
-                    path = "orders/{Query.filename}.json")
+                    path = "orders/{id}.json")
                     OutputBinding<String> outputItem,
             final ExecutionContext context) {
 
-        context.getLogger().info("Uploading file" + request.getQueryParameters().get("filename") + ".json");
+            outputItem.setValue(message);
 
-        if (request.getBody().isPresent()) {
-            outputItem.setValue(request.getBody().get());
-        }
     }
 }
